1e80ee5e1c4d93468aab46ae0ecdf2c25870fdb1
Phuong To <tophu95@gmail.com>
2019-05-21 21:32:24 +0100

    experiementing with animate css & js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let header = document.querySelector("h1");
+// header.innerHTML = "Hello World";
+
+// let sub = document.querySelectorAll("li");
+// iterate with a loop inside a function
+// sub.innerHTML = "bye";
